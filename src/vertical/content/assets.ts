dd6098baeeb88ac27d357f52b71cd9678adc5c73
Rostyslav Brenych <brenychinfo@gmail.com>
2026-03-02 20:37:57 +0100

    chore(atlas): assets folders + strict manifest

---
+++ src/vertical/content/assets.ts
@@ -0,0 +1,77 @@
+export const ATLAS_ASSETS = {
+  hero: {
+    hr01Morning16x9: "/atlas/hero/hr-01-morning-16x9.png",
+    hr02Sunset16x9: "/atlas/hero/hr-02-sunset-16x9.png",
+    hr03Bluehour16x9: "/atlas/hero/hr-03-bluehour-16x9.png",
+    hr04Mobile9x16: "/atlas/hero/hr-04-mobile-9x16.png",
+  },
+  renders: {
+    exterior: {
+      ex01Approach3x2: "/atlas/renders/exterior/ex-01-approach-3x2.png",
+      ex02Courtyard3x2: "/atlas/renders/exterior/ex-02-courtyard-3x2.png",
+      ex03Rooftop16x9: "/atlas/renders/exterior/ex-03-rooftop-16x9.png",
+      ex04Balconyview3x2: "/atlas/renders/exterior/ex-04-balconyview-3x2.png",
+      ex05Aerial16x9: "/atlas/renders/exterior/ex-05-aerial-16x9.png",
+    },
+    interior: {
+      in01Living3x2: "/atlas/renders/interior/in-01-living-3x2.png",
+      in02Kitchen3x2: "/atlas/renders/interior/in-02-kitchen-3x2.png",
+      in03Bedroom3x2: "/atlas/renders/interior/in-03-bedroom-3x2.png",
+      in04Bathroom3x2: "/atlas/renders/interior/in-04-bathroom-3x2.png",
+      in05Lobby16x9: "/atlas/renders/interior/in-05-lobby-16x9.png",
+    },
+    amenities: {
+      am01Gym16x9: "/atlas/renders/amenities/am-01-gym-16x9.png",
+      am02Spa16x9: "/atlas/renders/amenities/am-02-spa-16x9.png",
+    },
+  },
+  location: {
+    lo01Metro3x2: "/atlas/location/lo-01-metro-3x2.png",
+    lo02Park3x2: "/atlas/location/lo-02-park-3x2.png",
+    lo03Beach16x9: "/atlas/location/lo-03-beach-16x9.png",
+  },
+  plans: {
+    plT11br1x1: "/atlas/plans/pl-t1-1br-1x1.png",
+    plT22brStandard1x1: "/atlas/plans/pl-t2-2br-standard-1x1.png",
+    plT32brCorner1x1: "/atlas/plans/pl-t3-2br-corner-1x1.png",
+    plT43brFamily1x1: "/atlas/plans/pl-t4-3br-family-1x1.png",
+    plT53brPenthouse1x1: "/atlas/plans/pl-t5-3br-penthouse-1x1.png",
+    plT64brDuplex1x1: "/atlas/plans/pl-t6-4br-duplex-1x1.png",
+  },
+  masterplan: {
+    mp01Wide16x9: "/atlas/masterplan/mp-01-wide-16x9.png",
+    mp02Square1x1: "/atlas/masterplan/mp-02-square-1x1.png",
+  },
+  icons: {
+    ic00Grid1x1: "/atlas/icons/ic-00-grid-1x1.png",
+    ic01Wellness1x1: "/atlas/icons/ic-01-wellness-1x1.png",
+    ic02Kids1x1: "/atlas/icons/ic-02-kids-1x1.png",
+    ic03Parking1x1: "/atlas/icons/ic-03-parking-1x1.png",
+    ic04Green1x1: "/atlas/icons/ic-04-green-1x1.png",
+    ic05Security1x1: "/atlas/icons/ic-05-security-1x1.png",
+  },
+  og: {
+    og01Home1200x630: "/atlas/og/og-01-home-1200x630.png",
+    og02Units1200x630: "/atlas/og/og-02-units-1200x630.png",
+  },
+  brand: {
+    mark011x1: "/atlas/brand/mark-01-1x1.png",
+  },
+  downloads: {
+    brochureCoverEn4x5: "/atlas/downloads/brochure-cover-en-4x5.png",
+    pricelistCoverEn4x5: "/atlas/downloads/pricelist-cover-en-4x5.png",
+  },
+} as const;
+
+export type AtlasAssetPath = (typeof ATLAS_ASSETS)[keyof typeof ATLAS_ASSETS];
+
+export function asset(path: string): string {
+  return path;
+}
+
+export function maybeWithWebp(pngPath: string): { png: string; webp: string } {
+  return {
+    png: pngPath,
+    webp: pngPath.replace(/\.png$/i, ".webp"),
+  };
+}
